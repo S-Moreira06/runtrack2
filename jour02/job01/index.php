dc4acf2af277087068ac0fc3075e98fe30f63d87
Moreira_Stephane <moreira.s@outlook.com>
2024-07-04 16:48:46 +0200

    add jour02

---
+++ jour02/job01/index.php
@@ -0,0 +1,25 @@
+<?php 
+    $nbr = 1;
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <?php 
+        while($nbr <= 1337)
+        {
+            echo $nbr."<br>";
+            $nbr++;
+        }
+    
+    ?>
+    
+</body>
+</html>
